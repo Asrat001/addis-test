100204aa26e1ffdace8fd08f2d7a3dd3f84ba971
assrat007 <asratadane169@gmail.com>
2023-10-06 22:31:36 +0300

    final code

---
+++ wp-bootstrap-starter/header.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php 
+
+    // WordPress 5.2 wp_body_open implementation
+    if ( function_exists( 'wp_body_open' ) ) {
+        wp_body_open();
+    } else {
+        do_action( 'wp_body_open' );
+    }
+
+?>
+
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wp-bootstrap-starter' ); ?></a>
+    <?php if(!is_page_template( 'blank-page.php' ) && !is_page_template( 'blank-page-with-container.php' )): ?>
+
+      <?php if(is_front_page() && !get_theme_mod( 'header_banner_visibility' )): ?>
+      
+    <?php endif; ?>
+		<div class="p-5" style="background-color:#F1EFEF">
+			<div class="row">
+                <?php endif; ?>
---
+++ wp-bootstrap-starter/single.php
@@ -0,0 +1,50 @@
+<?php
+   get_header();
+   ?>
+<p> blogpost > proerty detail </p>
+<div class="p-3 " style="background-color:white">
+   <?php
+      while (have_posts()) :
+        the_post();
+      ?>
+   <div class="row p-0">
+      <div class="col">
+         <?php if (get_field('header_image')) : ?>
+         <img src="<?php echo get_field('header_image'); ?>" alt="Header Image" class="h-100 object-fit-cover">
+         <?php endif; ?>
+      </div>
+      <div class="col  " style="">
+         
+         <p> <?php echo get_the_content();?></p>
+         <p>Price: <?php echo get_field('price'); ?></p>
+         <div class="row w-100 " >
+            <div class="col">
+               <div class="card" style="background-color: #7D7C7C; border-radius: 0;">
+                  <div class="card-body px-2 py-0">
+                     <h5 class="card-title ">
+                        <i class="fas  fa-check-square text-white mr-3"></i> Fact sheet
+                     </h5>
+                     <p class="card-text text-center ml-5"> <?php echo  get_field('facts'); ?> </p>
+                  </div>
+               </div>
+            </div>
+            <div class="col">
+               <div class="card" style="background-color: #ADD8E6; border-radius: 0;">
+                  <div class="card-body px-2 py-0">
+                     <h5 class="card-title card-title ">
+                        <i class="fas fa-money-bill mr-3"></i> Payment plan
+                     </h5>
+                     <p class="card-text "> <?php echo  get_field('payment_plan'); ?> </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <?php
+      endwhile;
+      ?>
+</div>
+<?php
+   get_footer();
+   ?>
